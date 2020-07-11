a81c8d89128291325184ed64026983f43d1ffebe
Jianshu-Hu <jason-hu@jason-HudeMacBook-Pro.local>
2020-07-11 15:03:45 -0400

    update plotting function for landscape

--- data_process/data_process_find_boundary/test_color.py
+++ data_process/data_process_find_boundary/test_color.py
@@ -1,36 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors
-from mpl_toolkits import mplot3d
 
-x = 5 * np.random.random(10)
-y = 5 * np.random.random(10)
-z = 5 * np.random.random(10)
+# X = np.array([[0, 0], [1, 0.5], [1, 1.5], [0, 1], [0, 0]])
 
-levels = [0, 1, 2, 3, 4, 5]
-colors = ['red', 'brown', 'yellow', 'green', 'blue']
-cmap, norm = matplotlib.colors.from_levels_and_colors(levels, colors)
-cmap.set_over('orange')
-cmap.set_under('orange')
-
-fig, ax = plt.subplots()
-surf = ax.tricontourf(x, y, z, cmap=cmap, norm=norm, extend='both', levels=levels)
-cbar = fig.colorbar(surf, shrink=0.5, aspect=6, extend='both')
+Z = np.array([0, 1, 1, 0, 0])
+Z2 = np.array([0, 0.5, 1.5, 1, 0])
+X = np.ones((Z.shape[0],2))
+X[:,0] = Z
+X[:,1] = Z2
+t1 = plt.Polygon(X, color='red')
+plt.gca().add_patch(t1)
+plt.plot(Z, Z2, color='red')
 
 plt.show()
 
-# fig = plt.figure()
-# ax = plt.axes(projection="3d")
-#
-# z_line = np.array([1, 1, 1, 1, 1])
-# x_line = np.array([0, 0, 1, 1, 0])
-# y_line = np.array([0, 1, 1, 0, 0])
-# ax.plot3D(x_line, y_line, z_line, 'black', linewidth=2)
-#
-# z1_line = np.array([1, 1, 1, 1, 1])
-# x1_line = np.array([0, 0, 0.5, 0.5, 0])
-# y1_line = np.array([0, 0.5, 0.5, 0, 0])
-# ax.plot3D(x1_line, y1_line, z1_line, 'black')
-
-# ax.view_init(azim=0, elev=90)
-# plt.show()
